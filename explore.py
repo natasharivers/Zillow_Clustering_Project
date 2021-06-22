df89c0e895c1b9597df2005310777318bdfac809
Natasha Rivers <natasha.tanya.rivers@gmail.com>
2021-06-22 09:51:05 -0500

    add function for cluster and plotting

--- explore.py
+++ explore.py
@@ -197,3 +197,33 @@ def min_max_scaler(X_train, X_validate, X_test, numeric_cols):
         X_test[i] = X_test_scaled[i]
     return X_train, X_validate, X_test
 
+
+############################## CREATE CLUSTERS ##############################
+
+def create_cluster(df, X, k):
+    
+    """ Takes in df, X (dataframe with variables you want to cluster on) and k
+    # It scales the X, calcuates the clusters and return train (with clusters), the Scaled dataframe,
+    #the scaler and kmeans object and unscaled centroids as a dataframe"""
+    
+    scaler = StandardScaler(copy=True).fit(X)
+    X_scaled = pd.DataFrame(scaler.transform(X), columns=X.columns.values).set_index([X.index.values])
+    kmeans = KMeans(n_clusters = k, random_state = 42)
+    kmeans.fit(X_scaled)
+    kmeans.predict(X_scaled)
+    df['cluster'] = kmeans.predict(X_scaled)
+    df['cluster'] = 'cluster_' + df.cluster.astype(str)
+    centroids = pd.DataFrame(scaler.inverse_transform(kmeans.cluster_centers_), columns=X_scaled.columns)
+    return df, X_scaled, scaler, kmeans, centroids
+
+############################## SCATTERPLOT ##############################
+
+def create_scatter_plot(x,y,df,kmeans, X_scaled, scaler):
+    
+    """ Takes in x and y (variable names as strings, along with returned objects from previous
+    function create_cluster and creates a plot"""
+    
+    plt.figure(figsize=(10, 6))
+    sns.scatterplot(x = x, y = y, data = df, hue = 'cluster')
+    centroids = pd.DataFrame(scaler.inverse_transform(kmeans.cluster_centers_), columns=X_scaled.columns)
+    centroids.plot.scatter(y=y, x= x, ax=plt.gca(), alpha=.30, s=500, c='black')
