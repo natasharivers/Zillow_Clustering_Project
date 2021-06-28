26dc792511b95078ece1ba8742fd480f645be927
Natasha Rivers <natasha.tanya.rivers@gmail.com>
2021-06-28 11:45:17 -0500

    new functions

--- explore.py
+++ explore.py
@@ -116,8 +116,10 @@ def create_cluster(df, X, k):
 
 def create_scatter_plot(x,y,df,kmeans, X_scaled, scaler):
     
-    """ Takes in x and y (variable names as strings, along with returned objects from previous
-    function create_cluster and creates a plot"""
+    ''' 
+    Takes in x and y (variable names as strings, along with returned objects from previous
+    function create_cluster and creates a plot
+    '''
     
     plt.figure(figsize=(10, 6))
     sns.scatterplot(x = x, y = y, data = df, hue = 'cluster')
@@ -132,3 +134,52 @@ def plot_variable_pairs(df, cont_vars = 2):
         plt.figure(figsize=(8,3))
         sns.regplot(data=df, x=i[0], y =i[1],line_kws={"color":"black"})
         plt.show()
+
+############################## GROUP SCATTERPLOT  ##############################
+
+def scatter_plots(X_scaled, col_name= 'column_one', col_name_two= 'column_two'):
+    '''
+    This function takes in two columns and 
+    creates a range of scatter plots based on varying k values
+    '''
+    fig, axs = plt.subplots(2, 2, figsize=(10, 10), sharex=True, sharey=True)
+    for ax, k in zip(axs.ravel(), range(2, 6)):
+        clusters = KMeans(k).fit(X_scaled).predict(X_scaled)
+        ax.scatter(X_scaled[col_name], X_scaled[col_name_two], c=clusters)
+        ax.set(title='k = {}'.format(k), xlabel=col_name, ylabel=col_name_two)
+
+############################## CREATE SCATTERPLOT  ##############################
+
+def create_scatter_plot(x, y, df, kmeans, X_scaled, scaler, hue_column = 'hue_column'):
+    '''
+    This function takes in x and y as strings, 
+    and returns creates a plot
+    '''
+    plt.figure(figsize=(14, 9))
+    sns.scatterplot(x = x, y = y, data = df)
+    centroids = pd.DataFrame(scaler.inverse_transform(kmeans.cluster_centers_), columns=X_scaled.columns)
+    centroids.plot.scatter(y=y, x= x, ax=plt.gca(), alpha=.30, s=500, c='black')
+
+############################## CREATE CLUSTERS  ##############################
+
+def create_cluster(df, X, k, col_name = None):
+    
+    ''' 
+    This function takes in df, X (dataframe with variables you want to cluster on) and k
+    It scales the X, calcuates the clusters and return train (with clusters), the Scaled dataframe,
+    the scaler and kmeans object and scaled centroids as a dataframe
+    '''
+    scaler = StandardScaler(copy=True).fit(X)
+    X_scaled = pd.DataFrame(scaler.transform(X), columns=X.columns.values).set_index([X.index.values])
+    kmeans = KMeans(n_clusters = k, random_state = 123)
+    kmeans.fit(X_scaled)
+    centroids_scaled = pd.DataFrame(kmeans.cluster_centers_, columns = list(X))
+    
+    if col_name == None:
+        #clusters on dataframe 
+        df[f'clusters_{k}'] = kmeans.predict(X_scaled)
+    else:
+        df[col_name] = kmeans.predict(X_scaled)
+    
+    
+    return df, X_scaled, scaler, kmeans, centroids_scaled
